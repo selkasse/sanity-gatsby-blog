9544e3aba118e79eca4b033e90e91874cf66f8db
selkasse <selkasse@gmu.edu>
2020-12-21 23:18:47 -0500

    added code type to serializers.js

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import Figure from './Figure'
+import React from "react";
+import Figure from "./Figure";
 
 const serializers = {
   types: {
-    authorReference: ({node}) => <span>{node.author.name}</span>,
-    mainImage: Figure
-  }
-}
+    authorReference: ({ node }) => <span>{node.author.name}</span>,
+    mainImage: Figure,
+    code: ({ node }) => <code>{node.code}</code>,
+  },
+};
 
-export default serializers
+export default serializers;
