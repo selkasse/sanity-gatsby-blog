11b2725ba35630e1a57055e5584388bf62335f0e
selkasse <selkasse@gmu.edu>
2020-12-21 19:38:47 -0500

    add decorators to schema

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || '39ktwltf',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
-  }
-}
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "39ktwltf",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production",
+  },
+};
